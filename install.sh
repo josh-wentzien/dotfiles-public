e2e27feac56bcb5e0609a67830240e9ec3d5d48e
Josh Wentzien <jwentzien@qumulo.com>
2025-07-16 10:02:27 -0700

    updated version of nvim

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ mkdir -p "$HOME/.config"
 cp -r nvim "$HOME/.config/nvim"
 cp -r tmux "$HOME/.config/tmux"
 
+sudo apt remove -y neovim
+sudo add-apt-repository ppa:neovim-ppa/stable -y
 sudo apt update
 sudo apt install -y neovim tmux ripgrep
